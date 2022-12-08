640b16773540aa49a1b957bf2250bb06d6b81d7c
Salar Hafezi <salar.hfz.work@gmail.com>
2022-12-08 12:42:10 +0330

    comments

---
+++ comments/avoiding-comments.js
@@ -0,0 +1,10 @@
+/*
+* Avoiding comments with:
+* refactoring <-> By better code quality
+* using git <-> remove dates, authors and unused code
+* Clean comments with:
+* no visual markers
+* short & precise
+* declares a warning or amplification of importance
+* defines a specific business logic
+*/
